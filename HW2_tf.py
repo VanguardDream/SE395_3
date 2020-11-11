1e6bd60c19111d1efc64c335cddc912f20c61265
bong <doorebong@gmail.com>
2020-11-12 01:48:19 +0900

    tensorboard..

--- HW2_tf.py
+++ HW2_tf.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 import functions as func
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 lossgraph = True
+numoflayer = 2
 
 # Loader
 data_train, data_test = tf.keras.datasets.mnist.load_data()
@@ -21,30 +23,72 @@ image_test = image_test.astype('float32')
 # (image_train, label_train) = (image_train/255.0, label_train/255.0)
 # (image_test, label_test) = (image_test/255.0, label_test/255.0)
 
-# Model structure - 2 Layer CNN
-two_layer_model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2,2)),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(32,activation='relu'),
-    tf.keras.layers.Dense(10,activation='softmax')
-])
 
-two_layer_model.compile(
+if numoflayer == 2:
+    # Model structure - 2 Layer CNN
+    two_layer_model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2,2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(32,activation='relu'),
+        tf.keras.layers.Dense(10,activation='softmax')
+    ])
+
+    two_layer_model.compile(
+        optimizer='SGD',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    log_dir = "log/two_l/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    hist = two_layer_model.fit(
+        image_train, 
+        label_train, 
+        batch_size=16, 
+        epochs=1, 
+        validation_data=(image_test, label_test),
+        callbacks=[tensorboard_callback]
+    )
+
+
+else:
+    # Model structure - 3 Layer CNN
+    three_layer_model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(8, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2,2)),
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2,2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(32,activation='relu'),
+        tf.keras.layers.Dense(10,activation='softmax')
+    ])
+
+    three_layer_model.compile(
     optimizer='SGD',
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
-)
+    )
+    
+    log_dir = "log/three_l/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-hist = two_layer_model.fit(image_train, 
-    label_train, 
-    batch_size=16, 
-    epochs=10, 
-    validation_data=(image_test, label_test)
-)
+    hist = three_layer_model.fit(
+        image_train,
+        label_train,
+        batch_size=16,
+        epochs=4,
+        validation_data=(image_test, label_test),
+        callbacks=[tensorboard_callback]
+    )
 
+
+# Plot Loss Graph
 if lossgraph:
     fig, loss_ax = plt.subplots()
 
@@ -63,3 +107,11 @@ if lossgraph:
     acc_ax.legend(loc='lower left')
 
     plt.show()
+
+# Tensorboard image upload
+img = np.reshape(image_train[123], (-1, 28, 28, 1))
+
+file_writer = tf.summary.create_file_writer(log_dir)
+
+with file_writer.as_default():
+    tf.summary.image("Training data", img, step=0)
